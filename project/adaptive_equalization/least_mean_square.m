4369ad3a44ac73793aff7bff9cb3cf4a01ac09f3
ERIC STAHL <stahler@dc1.base.seas.upenn.edu>
2018-04-01 22:48:52 -0400

    updated array bounds

--- project/adaptive_equalization/least_mean_square.m
+++ project/adaptive_equalization/least_mean_square.m
@@ -1,7 +1,7 @@
 function w_n = least_mean_square(step_size, w_n, d_n, x_n, M)
 [ignore, x_n_length] = size(x_n);
 
-for i = 1:x_n_length-M
+for i = 1:x_n_length-M + 1
     y_n = w_n * fliplr(x_n(i:M-1 + i))';
     e_n = d_n(i) - y_n;
     w_n = w_n + (step_size*e_n).* fliplr(x_n(i:M-1 + i));
