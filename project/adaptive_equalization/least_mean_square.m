6a35aa0857984974c5db7c5f45c5de332a9b62e4
ERIC STAHL <stahler@dc1.base.seas.upenn.edu>
2018-04-02 22:43:14 -0400

    working part b a

--- project/adaptive_equalization/least_mean_square.m
+++ project/adaptive_equalization/least_mean_square.m
@@ -1,7 +1,8 @@
 function w_n = least_mean_square(step_size, w_n, d_n, x_n, M)
 [ignore, x_n_length] = size(x_n);
+x_n = [zeros(1,floor((M-1)/2)) x_n zeros(1,ceil((M-1)/2))];
 
-for i = 1:x_n_length-M + 1
+for i = 1:x_n_length
     y_n = w_n * fliplr(x_n(i:M-1 + i))';
     e_n = d_n(i) - y_n;
     w_n = w_n + (step_size*e_n).* fliplr(x_n(i:M-1 + i));
--- project/adaptive_equalization/partb_a.m
+++ project/adaptive_equalization/partb_a.m
@@ -2,12 +2,12 @@ rng('default');
 %%%
 %SETUP
 %%%
-SNR = 30; %SNR of additive white noise in dB
+SNR = 25; %SNR of additive white noise in dB
 s_length = 1000;
 t_delay = 2; %units of delay for adaptive filter
-step_size = .001;
+step_size = .01;
 lms_iters = 1;
-M = 23;
+M = 25;
 
 h_n = [0.3 1 0.7 0.3 .02];
 s_n = randi([0 1], 1, s_length); %generate random binary sequence
@@ -17,10 +17,8 @@ s_n(~s_n) = -1; %replace all 0s with -1s
 %d_n = circshift(s_n, t_delay);
 d_n = [zeros(1,t_delay) s_n(1:end-t_delay)];
 
-%%%For testing purposes, let's ignore SNR
-%x_n = awgn(conv(s_n, h_n, 'same'), SNR); %awgn expects our signals to be
-%in dB
-x_n = conv(s_n, h_n, 'same');
+x_n = awgn(conv(s_n, h_n, 'same'), SNR);
+
 
 %Training
 w_n = train_adaptive_filter(step_size, d_n, x_n, M, lms_iters);
