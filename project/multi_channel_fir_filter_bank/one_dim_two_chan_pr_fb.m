2362071f2956907037243698825901b5531ee822
ERIC STAHL <stahler@dc1.base.seas.upenn.edu>
2018-04-09 00:10:31 -0400

    first go at pr_fb. working on demodulation to baseband for compression now

---
+++ project/multi_channel_fir_filter_bank/one_dim_two_chan_pr_fb.m
@@ -0,0 +1,28 @@
+pr_fb_order = 21;
+n = 0:1:1024;
+f_low = 0.2*pi;
+f_high = 0.7*pi;
+f_p = 0.4;
+x_n = cos(f_low.*n) + cos(f_high.*n);
+
+[h0,h1,g0,g1] = firpr2chfb(pr_fb_order, f_p);
+
+hold on
+grid on
+x_h0_n = 0.5*conv(x_n,h0);
+x_h1_n = 0.5*conv(x_n,h1);
+x_h0_recon = conv(x_h0_n,g0);
+x_h1_recon = conv(x_h1_n,g1);
+x_recon = x_h0_recon + x_h1_recon;
+[x_ejw, w] = freqz(x_n);
+plot(w/pi,mag2db(abs(x_ejw)), '*');
+[x_ejw_low, w] = freqz(x_h0_n);
+plot(w/pi,mag2db(abs(x_ejw_low)));
+[x_ejw_high, w] = freqz(x_h1_n);
+plot(w/pi,mag2db(abs(x_ejw_high)));
+[x_ejw_reconstructed, w] = freqz(x_recon);
+plot(w/pi,mag2db(abs(x_ejw_reconstructed)), '.');
+
+xlabel('Normalized Frequency');
+ylabel('Magnitude_{dB}');
+legend('x(e^{jw})', 'x(e^{jw})_{lp}', 'x(e^{jw})_{hp}', 'x(e^{jw})_{re}');
