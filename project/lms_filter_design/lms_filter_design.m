12af78d393be902b1219a60c2a57367ad629b753
snazyman <sleepingzucchini@gmail.com>
2018-04-09 10:31:39 -0400

    Updated linear phase in 3. Updated Parameters in 1

--- project/lms_filter_design/lms_filter_design.m
+++ project/lms_filter_design/lms_filter_design.m
@@ -4,7 +4,7 @@
 
 clc;clear;close all;
 
-M = 128; % desired filter length
+M = 129; % desired filter length
 K = 1000; % length of input sequence
 L = linspace(0, .5, 100); % number of frequencies in input sequence
 h_lms = zeros([1,M]); % initial settings for h_lms
@@ -28,7 +28,10 @@ for i = (M+1):(length(x_in)-M-1)
     err(i) = (x_d(i-M+1) - y);
     
     % update coefficients
-    h_lms = h_lms + mu * x_in(i-M+1:i) * err(i);
+    %h_lms = h_lms + mu * x_in(i-M+1:i) * err(i);
+    % below enforces symmetry (which enforces linear phase)
+    h_lms(1:ceil(M/2)) = h_lms(1:ceil(M/2)) + mu * x_in(i-M+1:i-floor(M/2)) * err(i);
+    h_lms(ceil(M/2)+1:M) = fliplr(h_lms(1:floor(M/2)));
 end
 
 % plot impulse response
@@ -93,12 +96,23 @@ title('Error as a function of round')
 xlabel('round')
 ylabel('loss on each round')
 
+n = -floor(M/2):floor(M/2);
+[gd, w] = gdel(h_lms, n, M*100);
+[gd,w] = grpdelay(h_lms,1, M*100);
+figure;
+plot(w,gd)
+title('Group delay of learned filter');
+xlabel('frequency');
+ylabel('delay');
+
+
+
 %% Part b
 % different desired frequency response
 
 clc;clear;close all;
 
-M = 128; % desired filter length
+M = 129; % desired filter length
 K = 1000; % length of input sequence
 L = linspace(0, .5, 100); % number of frequencies in input sequence
 h_lms = zeros([1,M]); % initial settings for h_lms
@@ -122,7 +136,10 @@ for i = (M+1):(length(x_in)-M-1)
     err(i) = (x_d(i-M+1) - y);
     
     % update coefficients
-    h_lms = h_lms + mu * x_in(i-M+1:i) * err(i);
+    %h_lms = h_lms + mu * x_in(i-M+1:i) * err(i);
+    % below enforces symmetry (which enforces linear phase)
+    h_lms(1:ceil(M/2)) = h_lms(1:ceil(M/2)) + mu * x_in(i-M+1:i-floor(M/2)) * err(i);
+    h_lms(ceil(M/2)+1:M) = fliplr(h_lms(1:floor(M/2)));
 end
 
 % plot impulse response
@@ -136,6 +153,7 @@ ylabel('amplitude');
 w = linspace(-pi, pi, K*2);
 H = fftshift(abs(fft(x_in,K*2)));
 figure;
+subplot(2,1,1)
 plot(w,H);
 title('Magnitude response of input signal');
 xlabel('frequency (radians)');
@@ -144,8 +162,7 @@ ylabel('amplitude');
 % compute and plot frequency response of desired signal
 w = linspace(-pi, pi, K*2);
 H = fftshift(abs(fft(x_d,K*2)));
-figure;
-subplot(2,1,1)
+subplot(2,1,2)
 plot(w,H);
 title('Magnitude response of desired signal');
 xlabel('frequency (radians)');
@@ -167,11 +184,16 @@ title('phase response of learned filter');
 xlabel('frequency (radians)');
 ylabel('angle');
 
-% plot error as a function of iterations
-figure;
-plot(err)
-title('Error as a function of round')
-xlabel('round')
-ylabel('loss on each round')
+%n = -floor(M/2):floor(M/2)
+%[gd, w] = gdel(h_lms, n, M*100);
+[gd,w] = grpdelay(h_lms,1, M*100);
+%w = linspace(-pi,pi,M*10)
+%h_ejw = fft(h_lms,M*10);
+%gd = polyfit(w,unwrap(angle(h_ejw)),1);
 
+figure;
+plot(w,gd)
+title('Group delay of learned filter');
+xlabel('frequency');
+ylabel('delay');
 
