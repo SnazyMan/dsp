cc48eeb4d4d8676b45081bab914e33fe11e1b8b7
ERIC STAHL <stahler@dc1.base.seas.upenn.edu>
2018-04-11 23:26:22 -0400

    working part 3. still need to finish final part

--- project/multi_channel_fir_filter_bank/pr_fb_decimated.m
+++ project/multi_channel_fir_filter_bank/pr_fb_decimated.m
@@ -44,10 +44,9 @@ x_hh_n = imresize(x_hh_temp(T-M/2:V-M/2, T-M/2:V-M/2), 0.5);
 x_lh_n = imresize(x_lh_temp(T-M/2:V-M/2, T-M/2:V-M/2), 0.5);
 x_hl_n = imresize(x_hl_temp(T-M/2:V-M/2, T-M/2:V-M/2), 0.5);
 
-norm_ll_n = 0.25*conv2(h0,h0,img_man,'same');
-norm_hh_n = 0.25*conv2(h1,h1,img_man,'same');
-norm_lh_n = 0.25*conv2(h1,h0,img_man, 'same');
-norm_hl_n = 0.25*conv2(h0,h1,img_man, 'same');
+x_hh_n = x_hh_n.*0;
+x_lh_n = x_lh_n.*0;
+x_hl_n = x_hl_n.*0;
 
 y_ll_temp = conv2(g0,g0,imresize(x_ll_n,2));
 y_hh_temp = conv2(g1,g1,demod_exp_up.*(demod_exp_up.*imresize(x_hh_n,2))');
@@ -62,66 +61,7 @@ y_hl_n = y_hl_temp(X+M/2:W+M/2, X+M/2:W+M/2);
 x_recon = y_ll_n + y_hh_n + y_lh_n + y_hl_n;
 
 x_ejw = fft2(img_man);
-x_ll_ejw = fft2(x_ll_n);
-x_hh_ejw = fft2(x_hh_n);
-x_lh_ejw = fft2(x_lh_n);
-x_hl_ejw = fft2(x_hl_n);
-
-norm_ll_ejw = fft2(norm_ll_n);
-norm_hh_ejw = fft2(norm_hh_n);
-norm_lh_ejw = fft2(norm_lh_n);
-norm_hl_ejw = fft2(norm_hl_n);
-
-y_ll_ejw = fft2(y_ll_n);
-y_hh_ejw = fft2(y_hh_n);
-y_lh_ejw = fft2(y_lh_n);
-y_hl_ejw = fft2(y_hl_n);
-
 x_recon_ejw = fft2(x_recon);
-
-figure;
-subplot(2,2,1);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(y_ll_ejw))));
-colormap(gray);
-
-subplot(2,2,2);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(y_hh_ejw))));
-colormap(gray);
-
-subplot(2,2,3);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(y_lh_ejw))));
-colormap(gray);
-
-subplot(2,2,4);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(y_hl_ejw))));
-colormap(gray);
-
-figure;
-subplot(2,2,1);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(norm_ll_ejw))));
-colormap(gray);
-
-subplot(2,2,2);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(norm_hh_ejw))));
-colormap(gray);
-
-subplot(2,2,3);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(norm_lh_ejw))));
-colormap(gray);
-
-subplot(2,2,4);
-imagesc([-1,1],[-1,1],100*log(1+ abs(fftshift(norm_hl_ejw))));
-colormap(gray);
-
-figure;
-subplot(2,2,1);
-imshow(4.*abs(x_ll_n));
-subplot(2,2,2);
-imshow(4.*abs(x_hh_n));
-subplot(2,2,3);
-imshow(4.*abs(x_lh_n));
-subplot(2,2,4);
-imshow(4.*abs(x_hl_n));
 
 figure;
 subplot(2,2,1);
